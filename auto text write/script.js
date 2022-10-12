3db688da92b8518dae93574950c6ef40a978f78b
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-12 14:32:11 +0530

    texte writer

---
+++ auto text write/script.js
@@ -0,0 +1,15 @@
+let text = "welcome to this page .";
+
+let index = 0 ;
+
+function writeText (){
+    document.body.innerText = `${text.slice(0 , index)}|` ; 
+    index ++ ; 
+    if(index>text.length-1){
+        index = 0 
+    }
+}
+
+setInterval(()=>{
+    writeText() 
+},200);
