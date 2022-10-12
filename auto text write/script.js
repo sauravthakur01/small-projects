df46c6d788c7f6ee48a93d374aa94e91f42f8170
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-12 15:29:22 +0530

    popup modal

--- auto text write/script.js
+++ auto text write/script.js
@@ -5,7 +5,7 @@ let index = 0 ;
 function writeText (){
     document.body.innerText = `${text.slice(0 , index)}|` ; 
     index ++ ; 
-    if(index>text.length-1){
+    if(index>text.length){
         index = 0 
     }
 }
